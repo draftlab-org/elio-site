f74953068a82f4a098727aff9276b3ffa4176384
Tin Geber <tingeber@gmail.com>
2025-05-01 23:30:00 +0200

    basic layout, hero, poop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.bunny(),
+        name: "Petrona",
+        cssVariable: "--font-petrona",
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+      {
+        provider: fontProviders.bunny(),
+        name: "Outfit",
+        cssVariable: "--font-outfit",
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+    ],
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
