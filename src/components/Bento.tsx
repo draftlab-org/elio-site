9bd901de08ead3a58642d7cca5614d95d3a7ee17
Tin Geber <tingeber@gmail.com>
2025-05-04 15:27:13 +0200

    added Biome for linting

--- src/components/Bento.tsx
+++ src/components/Bento.tsx
@@ -1,6 +1,7 @@
-import { marked } from "marked";
-import { getCollection, getEntry } from "astro:content";
 import { twMerge } from "tailwind-merge";
+
+import { getCollection, getEntry } from "astro:content";
+import { marked } from "marked";
 
 // const styles = [
 //   {
@@ -25,35 +26,35 @@ export default function Bento() {
     <section id="bento-grid" className="">
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
         <div className="mt-10 grid gap-6 sm:mt-16 lg:grid-cols-2 lg:grid-rows-16">
-          {portfolio &&
-            portfolio.map((item, index) => (
-              <div
-                className={twMerge(
-                  "col-span-1 row-span-6 flex flex-col gap-8 rounded-2xl border-[1px] border-black p-6",
-                  `bg-brand-${item.data.background.primary_color}`,
-                )}
-              >
-                <p className="text-xl font-light">
-                  <span className="font-semibold">{item.data.id}</span> /
-                  {item.data.description}
-                </p>
-                <div className="tags mt-2 inline-flex gap-2">
-                  <span className="inline-flex items-center rounded-md bg-black/10 px-2 py-1 text-base font-medium text-black">
-                    {item.data.role}
-                  </span>
-                  <span className="inline-flex items-center rounded-md bg-black/10 px-2 py-1 text-base font-medium text-black">
-                    {item.data.period}
-                  </span>
-                </div>
-                <div className="mx-12">
-                  <img
-                    src={item.data.image.src.src}
-                    alt={item.data.id}
-                    className="w-full object-cover"
-                  />
-                </div>
+          {portfolio?.map((item, index) => (
+            <div
+              key={item.id}
+              className={twMerge(
+                "col-span-1 row-span-6 flex flex-col gap-8 rounded-2xl border-[1px] border-black p-6",
+                `bg-brand-${item.data.background.primary_color}`,
+              )}
+            >
+              <p className="text-xl font-light">
+                <span className="font-semibold">{item.data.id}</span> /
+                {item.data.description}
+              </p>
+              <div className="tags mt-2 inline-flex gap-2">
+                <span className="inline-flex items-center rounded-md bg-black/10 px-2 py-1 text-base font-medium text-black">
+                  {item.data.role}
+                </span>
+                <span className="inline-flex items-center rounded-md bg-black/10 px-2 py-1 text-base font-medium text-black">
+                  {item.data.period}
+                </span>
               </div>
-            ))}
+              <div className="mx-12">
+                <img
+                  src={item.data.image.src.src}
+                  alt={item.data.id}
+                  className="w-full object-cover"
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
